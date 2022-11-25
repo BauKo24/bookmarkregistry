8fcc945c9c0a7310defe6f35108b6d74aaaa060c
Ethan LERIGAB <ethanlerigab@gmail.com>
2022-11-25 09:13:42 +0100

    remise à zéro

--- backup/web/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-    $db = new PDO('mysql:host=localhost;dbname=brief4_db', 'root','');
-    $db->exec('SET NAMES "UTF8"');
-} catch (PDOException $e){
-    echo 'Erreur : '. $e->getMessage();
-    die();
-}
