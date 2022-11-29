c1f0e4b66e158cd5f1d5a3bb4598755282656ac8
Ethan LERIGAB <ethanlerigab@gmail.com>
2022-11-29 16:48:10 +0100

    "il serait peut-être temps de passer à docker." -Kristen

--- web/index.php
+++ web/index.php
@@ -1,5 +1,22 @@
 <?php
 require_once('connect.php');
 
-$controller->index();
+// $controller->index();
 
+$router = new AltoRouter();
+
+// map homepage
+$router->map( 'GET', '/', function() use ($controller) {
+	$controller->index();
+});
+
+// match current request url
+$match = $router->match();
+
+// call closure or throw 404 status
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
